56beb9540f33d0e63d5264a16f08dd1a4715236c
Nicolas Favre <nicolas.f@jaipasd.email>
2024-01-27 21:36:27 +0100

    V0.1.0

---
+++ index.js
@@ -0,0 +1,95 @@
+const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand');
+
+const appEnv = dotenv.config();
+dotenvExpand.expand(appEnv);
+
+const axios = require("axios");
+const { io } = require('socket.io-client');
+
+const { getObjects } = require('./libraries/utils');
+
+let monitorsList = {}
+
+// Kerner API Instance
+const kener = axios.create({
+    baseURL: process.env.KENER_URL,
+    timeout: 1000,
+    headers: {
+        'Authorization': `Bearer ${process.env.KENER_TOKEN}`,
+        'Content-Type' : 'application/json'
+    }
+});
+const updateStatus = (heart, tag, maxPing) => {
+    kener.post('/api/status', {
+        "status": ((heart.status === 1) ? ((heart.ping  > parseInt(maxPing)) ? 'DEGRADED' : 'UP') : 'DOWN'),
+        "latency": heart.ping,
+        "tag": tag
+    })
+        .then(function (response) {
+            const monitor = getObjects(monitorsList,'id', heart.monitor_id || heart.monitorID).find(item => item.type === 'push' || item.type === 'http' || item.type === 'port');
+            console.log(`${monitor.name} Updated status to kener (maxPing: ${maxPing})`);
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
+// Uptime Kuma Socket.io
+const kuma = io(process.env.KUMA_URL);
+
+kuma.on("connect", () => {
+  console.log('connected')
+  kuma.emit("login", { username: process.env.KUMA_USER,  password: process.env.KUMA_PASS,  token: null }, (res) => {
+        if (res.tokenRequired) {
+            console.log('Error 2FA enabled on this account');
+            return kuma.disconnect();
+        }
+
+        if (res.ok) {
+            kuma.uptime_kuma_token = res.token;
+            console.log('Logged on socket');
+        } else {
+            console.log('An error has occurred');
+            console.log(res, 'error');
+            return kuma.disconnect();
+        }
+    });
+})
+
+kuma.on("connect_error", (err) => {
+   console.log(`Failed to connect to the backend. Socket.io connect_error: ${err.message}`);
+});
+
+kuma.on("disconnect", () => {
+    console.log(`disconnected from the socket server `);
+});
+
+kuma.on("monitorList", async (monitors) => {
+    console.log(`Update Uptime Kuma monitors`);
+
+    monitorsList = monitors;
+});
+
+kuma.on("heartbeatList", (monitorID, data, overwrite = false) => {
+    const monitor = getObjects(monitorsList,'id', monitorID).find(item => item.type === 'push' || item.type === 'http' || item.type === 'port');
+    if (monitor === undefined) return;
+
+    const tag = monitor.tags.find(item => item.name === 'Kener');
+    const maxPing = monitor.tags.find(item => item.name === 'MaxPing');
+    if (tag !== null && tag !== undefined) {
+        const heart = data[data.length-1];
+        updateStatus(heart, tag.value, ((maxPing !== null && maxPing !== undefined) ? maxPing.value : 2000));
+    }
+});
+
+kuma.on("heartbeat", (data) => {
+    const monitor = getObjects(monitorsList,'id', data.monitorID).find(item => item.type === 'push' || item.type === 'http' || item.type === 'port');
+    if (monitor === undefined) return;
+
+    const tag = monitor.tags.find(item => item.name === 'Kener');
+    const maxPing = monitor.tags.find(item => item.name === 'MaxPing');
+    if (tag !== null && tag !== undefined) {
+        updateStatus(data, tag.value, ((maxPing !== null && maxPing !== undefined) ? maxPing.value : 2000));
+    }
+});
