2f254c4c87a7c5ba97d92febe5904570b83b4fb4
Nicolas Favre <nicolas.f@jaipasd.email>
2024-03-02 06:35:23 +0100

    Some fix

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const dotenv = require('dotenv');
 const dotenvExpand = require('dotenv-expand');
 
+
 const appEnv = dotenv.config();
 dotenvExpand.expand(appEnv);
 
+const Logger = require("@ptkdev/logger");
 const axios = require("axios");
+const { CronJob } = require('cron');
 const { io } = require('socket.io-client');
 
 const { getObjects } = require('./libraries/utils');
+
+const logger = new Logger();
 
 const debug = (msg) => {
     if (!process.env.DEBUG) return;
     console.log(msg)
 };
 let monitorsList = {}
+let lastHeartbeat = {}
 
 // Kerner API Instance
 const kener = axios.create({
@@ -25,16 +31,17 @@ const kener = axios.create({
     }
 });
 const updateStatus = (heart, tag, maxPing) => {
-    debug('Start kenet monitor update')
+    logger.info("Start monitor update", "Kener");
     kener.post('/api/status', {
         "status": ((heart.status === 1) ? ((heart.ping  > parseInt(maxPing)) ? 'DEGRADED' : 'UP') : 'DOWN'),
-        "latency": heart.ping,
+        "latency": heart.ping || 0,
+        "timestampInSeconds": (Date.now() / 1000),
         "tag": tag
     })
         .then(function (response) {
-            const monitor = getObjects(monitorsList,'id', heart.monitor_id || heart.monitorID).find(item => item.type === 'push' || item.type === 'http' || item.type === 'port');
-            console.log(`${monitor.name} Updated status to kener (maxPing: ${maxPing})`);
+            const monitor = getObjects(monitorsList,'id', heart.monitor_id || heart.monitorID).find(item => item.type === 'push' || item.type === 'http' || item.type === 'port' || item.type === 'gamedig');
 
+            logger.info(`${monitor.name} Updated status (maxPing: ${maxPing})`, "Kener");
         })
         .catch(function (error) {
             debug(error)
@@ -46,18 +53,18 @@ debug(`Kener url: ${process.env.KENER_URL}`);
 const kuma = io(process.env.KUMA_URL);
 
 kuma.on("connect", () => {
-  console.log('connected')
-  kuma.emit("login", { username: process.env.KUMA_USER,  password: process.env.KUMA_PASS,  token: null }, (res) => {
+    logger.info("Connected to socket", "UptimeKuma");
+    kuma.emit("login", { username: process.env.KUMA_USER,  password: process.env.KUMA_PASS,  token: null }, (res) => {
         if (res.tokenRequired) {
-            console.log('Error 2FA enabled on this account');
+            logger.error("Error 2FA enabled on this account", "UptimeKuma");
             return kuma.disconnect();
         }
 
         if (res.ok) {
             kuma.uptime_kuma_token = res.token;
-            console.log('Logged on socket');
+            logger.info("Logged In", "UptimeKuma");
         } else {
-            console.log('An error has occurred');
+            logger.error("An error has occurred", "UptimeKuma");
             console.log(res, 'error');
             return kuma.disconnect();
         }
@@ -65,38 +72,83 @@ kuma.on("connect", () => {
 })
 
 kuma.on("connect_error", (err) => {
-   console.log(`Failed to connect to the backend. Socket.io connect_error: ${err.message}`);
+    logger.error(`Failed to connect to the backend. Socket.io connect_error: ${err.message}`, "UptimeKuma");
 });
 
 kuma.on("disconnect", () => {
-    console.log(`disconnected from the socket server `);
+    logger.error(`disconnected from the socket server`, "UptimeKuma");
 });
 
 kuma.on("monitorList", async (monitors) => {
-    console.log(`Update Uptime Kuma monitors`);
-
+    logger.info(`Receive ${Object.keys(monitors).length} Monitors from UptimeKuma `, "Bridge");
     monitorsList = monitors;
 });
 
 kuma.on("heartbeatList", (monitorID, data, overwrite = false) => {
-    const monitor = getObjects(monitorsList,'id', monitorID).find(item => item.type === 'push' || item.type === 'http' || item.type === 'port');
-    if (monitor === undefined) return;
+    const monitor = getObjects(monitorsList,'id', monitorID).find(item => item.type === 'push' || item.type === 'http' || item.type === 'port' || item.type === 'gamedig');
 
+    logger.info(`Receive list for monitor #${monitorID} (${monitor.name})`, "Heartbeat List");
+
+    if (monitor === undefined) return;
     const tag = monitor.tags.find(item => item.name === 'Kener');
     const maxPing = monitor.tags.find(item => item.name === 'MaxPing');
     if (tag !== null && tag !== undefined) {
         const heart = data[data.length-1];
+        lastHeartbeat[monitorID] = heart;
+        lastHeartbeat[monitorID]['timestamp'] = Date.now();
         updateStatus(heart, tag.value, ((maxPing !== null && maxPing !== undefined) ? maxPing.value : 2000));
     }
 });
 
 kuma.on("heartbeat", (data) => {
-    const monitor = getObjects(monitorsList,'id', data.monitorID).find(item => item.type === 'push' || item.type === 'http' || item.type === 'port');
-    if (monitor === undefined) return;
+    const monitor = getObjects(monitorsList,'id', data.monitorID).find(item => item.type === 'push' || item.type === 'http' || item.type === 'port' || item.type === 'gamedig');
 
+    logger.info(`Receive for monitor #${data.monitorID} (${monitor.name})`, "Heartbeat");
+    if (monitor === undefined) return;
     const tag = monitor.tags.find(item => item.name === 'Kener');
+    lastHeartbeat[data.monitorID] = data;
+    lastHeartbeat[data.monitorID]['timestamp'] = Date.now();
     const maxPing = monitor.tags.find(item => item.name === 'MaxPing');
     if (tag !== null && tag !== undefined) {
         updateStatus(data, tag.value, ((maxPing !== null && maxPing !== undefined) ? maxPing.value : 2000));
     }
+});
+
+kuma.on("uptime", (monitorID, dd, gg) => {
+    const monitor = getObjects(monitorsList,'id', monitorID).find(item => item.type === 'push' || item.type === 'http' || item.type === 'port' || item.type === 'gamedig');
+
+    logger.info(`Receive for monitor #${monitorID} (${monitor.name})`, "Uptime");
+    const lh = lastHeartbeat[monitorID]
+    if (lh) {
+        const cur = Date.now();
+        const tag = monitor.tags.find(item => item.name === 'Kener')
+        const maxPing = monitor.tags.find(item => item.name === 'MaxPing');
+        if ((cur - lh.timestamp) >= 30 && tag !== undefined) {
+            console.log((cur - lh.timestamp))
+            updateStatus(lh, tag.value, ((maxPing !== null && maxPing !== undefined) ? maxPing.value : 2000));
+            lastHeartbeat[monitorID]['timestamp'] = Date.now();
+        }
+    }
+});
+
+const job = CronJob.from({
+	cronTime: '0 */1 * * * *',
+	onTick: function () {
+
+    logger.info(`Cron as triggered`, "Cron");
+        for (const mon in lastHeartbeat) {
+            const heartbeat = lastHeartbeat[mon];
+
+            const monitor = getObjects(monitorsList,'id', heartbeat.monitorID).find(item => item.type === 'push' || item.type === 'http' || item.type === 'port' || item.type === 'gamedig');
+            if (monitor !== null && monitor !== undefined) {
+                const tag = monitor.tags.find(item => item.name === 'Kener')
+                const maxPing = monitor.tags.find(item => item.name === 'MaxPing');
+                if (tag !== null && tag !== undefined) {
+                    updateStatus(heartbeat, tag.value, ((maxPing !== null && maxPing !== undefined) ? maxPing.value : 2000));
+                }
+            }
+        }
+	},
+	start: true,
+	timeZone: 'Europe/Paris'
 });
